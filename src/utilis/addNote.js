036cce19a22ed5b766fa1092e8630ede1062a517
Rishi Chourasia <rishichourasia2100@gmail.com>
2022-05-02 15:21:48 +0530

    [fix] Removed logs

--- src/utilis/addNote.js
+++ src/utilis/addNote.js
@@ -19,14 +19,12 @@ const addNotes = async (dispatchNotes, note) => {
 const updateNotes = async (dispatchNotes, note) => {
 	const authToken = localStorage.getItem("token");
 	const Headers = { authorization: authToken };
-	console.log(note._id);
 	const noteObj = { note: note };
 
 	try {
 		const response = await axios.post(`/api/notes/${note._id}`, noteObj, {
 			headers: Headers,
 		});
-		console.log(response.data.notes);
 		dispatchNotes({ type: "FETCH_NOTES", payload: response.data.notes });
 	} catch (error) {
 		console.log(error.message);
